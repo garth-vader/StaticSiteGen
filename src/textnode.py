bf9f9b4df45a0bde6f49c9ee8a2ee76d88b40f97
garth-vader <drago.shinsengumi@gmail.com>
2026-04-03 00:30:32 -0400

    Added TextNode

---
+++ src/textnode.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "**Bold text**"
+    ITALIC = "_Italic text_"
+    CODE = "`Code text`"
+    LINK = "link"
+    IMAGE = "![alt text](url)"
+
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        if not url:
+            self.url = None
+        else: 
+            self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self):
+
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
