451d07911f09a827a437ca108c1447a6ee056aeb
garth-vader <drago.shinsengumi@gmail.com>
2026-05-05 15:34:56 -0400

    added blog and contact content

--- src/main.py
+++ src/main.py
@@ -5,4 +5,8 @@ def main():
     copy_folder("./static/", "./public/")
     generate_page("./content/index.md", "./template.html", "./public/index.html")
 
+    generate_page("./content/blog/glorfindel/index.md", "./template.html", "./public/blog/glorfindel/index.html")
+    generate_page("./content/blog/tom/index.md", "./template.html", "./public/blog/tom/index.html")
+    generate_page("./content/blog/majesty/index.md", "./template.html", "./public/blog/majesty/index.html")
+    generate_page("./content/contact/index.md", "./template.html", "./public/contact/index.html")
 main()
