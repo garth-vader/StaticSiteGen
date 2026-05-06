1a02eb1d8de2dbf8171baf998eb350133a83e593
garth-vader <drago.shinsengumi@gmail.com>
2026-05-05 22:52:46 -0400

    changed /docs/

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from copystatic import copy_folder, delete_public
 from generate_page import generate_page_recursive
-
+import os
+import shutil
 import sys
 
 source_path = "./content/"
@@ -9,10 +10,17 @@ destination_path = "./docs/"
 static_path = "./static/"
 
 def main():
-    basepath = sys.argv
+    basepath = ""
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     if basepath == "":
         basepath = '/'
     
+
+    if os.path.exists(destination_path):
+        shutil.rmtree(destination_path)
+    else:
+        print("public is already deleted")
     # delete_public()
 
     copy_folder(static_path, destination_path)
