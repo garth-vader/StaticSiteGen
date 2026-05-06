eed9408c37201c9db15913689797d278a99b25a3
garth-vader <drago.shinsengumi@gmail.com>
2026-05-05 20:58:34 -0400

    setting up for github page

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,7 +1,7 @@
 import os
 from markdown_blocks import markdown_to_html_node
 from extract_title import extract_title_text
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
 
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r", encoding="utf-8") as f:
@@ -14,7 +14,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title_text(src_markdown)
      
     page = template_text.replace(r"{{ Title }}", title).replace(r"{{ Content }}", content)
-
+    page = page.replace("href=\"/", f"href=\"{basepath}")
+    page = page.replace("src=\"/", f"src=\"{basepath}")
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
 
@@ -23,14 +24,14 @@ def generate_page(from_path, template_path, dest_path):
 
     print(f"wrote {amount_written}") 
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if os.path.isfile(dir_path_content) and dir_path_content.endswith(".md"):
         dest = dest_dir_path.replace(".md", ".html")
-        generate_page(dir_path_content, template_path, dest)
+        generate_page(dir_path_content, template_path, dest, basepath)
         return 
     for f in os.listdir(dir_path_content):
         new_dir_path = os.path.join(dir_path_content, f)
         new_dest_path = os.path.join(dest_dir_path, f)
-        generate_page_recursive(new_dir_path, template_path, new_dest_path)
+        generate_page_recursive(new_dir_path, template_path, new_dest_path, basepath)
 
 
--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
 from copystatic import copy_folder, delete_public
 from generate_page import generate_page_recursive
-def main():
-    delete_public()
-    copy_folder("./static/", "./public/")
-    generate_page_recursive("./content/", "./template.html", "./public/")
 
-    # generate_page("./content/blog/glorfindel/index.md", "./template.html", "./public/blog/glorfindel/index.html")
-    # generate_page("./content/blog/tom/index.md", "./template.html", "./public/blog/tom/index.html")
-    # generate_page("./content/blog/majesty/index.md", "./template.html", "./public/blog/majesty/index.html")
-    # generate_page("./content/contact/index.md", "./template.html", "./public/contact/index.html")
+import sys
+
+source_path = "./content/"
+template_path = "./template.html"
+destination_path = "./docs/"
+static_path = "./static/"
+
+def main():
+    basepath = sys.argv
+    if basepath == "":
+        basepath = '/'
+    
+    # delete_public()
+
+    copy_folder(static_path, destination_path)
+    generate_page_recursive(source_path, template_path, destination_path, basepath)
+
 main()
